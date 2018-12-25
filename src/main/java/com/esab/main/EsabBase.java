e94b42e2decd37bface3b52f7f22e693c8a7c0a7
nkkishore2789 <nkkishore89@gmail.com>
2018-12-25 09:18:54 +0530

    push to esab

---
+++ src/main/java/com/esab/main/EsabBase.java
@@ -0,0 +1,33 @@
+package com.esab.main;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+public class EsabBase {
+	
+	@Test
+	public static void esablaunch() throws MalformedURLException{
+		AndroidDriver<MobileElement> driver;
+		DesiredCapabilities cap=new DesiredCapabilities();
+		cap.setCapability("deviceName", "OnePlus 6");
+		cap.setCapability(CapabilityType.BROWSER_VERSION, "");
+		cap.setCapability(CapabilityType.PLATFORM_NAME, "Android");
+		cap.setCapability(CapabilityType.VERSION, "9");
+		cap.setCapability("appPackage", "com.esab.weldclouldscanner");
+		cap.setCapability("appActivity","com.esab.weldclouldscanner.Activities.SplashScreen");
+		
+		driver=new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+		
+	}
+	
+	
+	
+
+}
